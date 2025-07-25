d486fe9c3e06d527ea756d5849476f023f36ecbb
rushabh <rushabhzode84@gmail.com>
2025-07-25 16:50:39 +0530

    learn data types

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+let name = "Rushabh"; //string data type
+let age = 20; //number data type
+let isloggedIn = true/fase; //boolean data type
+let state = null; //null data type(standalone value)
+let bigintnumber = BigInt(12345678901234567890);// use for big numbers
+let sym = Symbol("I am a symbol"); //symbol data type (unique value)
+let notDefined; //undefined data type (variable is declared but not assigned)
+let typeof name; //returns the type of variable
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object (this is a known bug in JavaScript)
