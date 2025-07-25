e2c204c1908c686af6d8626f2fa43b9e270683cc
rushabh <rushabhzode84@gmail.com>
2025-07-25 15:41:50 +0530

    first java script code

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId =12345
+let accountEmail = "rushabhzode@gmail.com"
+var accountPassword = "12345"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword]);
