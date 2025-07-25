1de783af31c9f8975061e0612dc7b9f03e915a53
rushabh <rushabhzode84@gmail.com>
2025-07-25 17:29:38 +0530

    Code of ConversionOperation

---
+++ basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "Rushabh";
+
+console.log(typeof(score));
+//console.log(typeofscore);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1
+
+
+let isLoggedIn = "rushabh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
