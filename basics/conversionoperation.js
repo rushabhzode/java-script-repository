6f26e8d72af2ef6e36a146bbb416efb46001b7e9
rushabh <rushabhzode84@gmail.com>
2025-07-26 17:56:57 +0530

    update conversionoperation

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -1,22 +1,53 @@
-let score = "Rushabh";
+//let score = "Rushabh";
 
-console.log(typeof(score));
+//console.log(typeof(score));
 //console.log(typeofscore);
 
-let valueInNumber = Number(score);
-console.log(valueInNumber);
+//let valueInNumber = Number(score);
+//console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
 // true => 1
 
 
-let isLoggedIn = "rushabh";
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn); 
+//let isLoggedIn = "rushabh";
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn); 
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+//let someNumber = 33;
+//let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof(stringNumber));
+
+////**********operations*****************
+
+//console.log(3+3);
+//console.log(3-3);
+//console.log(3*3);
+//console.log(3/3);
+//console.log(3%3);
+
+
+//let str1= "hello";
+//let str2 = " Rushabh";
+//str3 = str1 + str2;
+//console.log(str3);
+
+
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + "2");
+//console.log(1 + 2 + "3");
+//console.log("1" + 2 + 3);
+//console.log(1 + 2 + "3" + 4);
+
+//let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter); 
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
