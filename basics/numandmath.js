99c39208bda9eef714488e1e1cbe4168f38f27c2
rushabh <rushabhzode84@gmail.com>
2025-07-28 15:11:17 +0530

    Learn number and math methods

---
+++ basics/numandmath.js
@@ -0,0 +1,15 @@
+// const score = 100;
+// console.log(score);
+
+// //this is No.data type value but if i want perfect No. data type
+
+// const percentage = new Number(100);
+// console.log(percentage);
+
+//********************************************
+
+// const math = 400;
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
