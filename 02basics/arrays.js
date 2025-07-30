6b4fae0057aa0fb7f35d798dc9b0545fc2398ea1
rushabh <rushabhzode84@gmail.com>
2025-07-30 17:34:49 +0530

    learn basics arrays and its methods

---
+++ 02basics/arrays.js
@@ -0,0 +1,24 @@
+//Array
+
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr);
+
+// const myHeroes = ["batman", "superman", "spiderman",];
+// console.log(myHeroes);
+
+// const myarr2 = new Array(1,2,3,4,5);
+// console.log(myarr2[3]);
+
+
+//Array Methods
+
+
+const myArr = [1,2,3,4,5,6,7,8,9,10];
+// myArr.push(11); // Adds an element to the end of the array
+// myArr.pop(); // Removes the last element from the array
+//myArr.unshift(7); // Adds an element to the beginning of the array
+//myArr.shift(); // Removes the first element from the array
+// console.log(myArr.includes(5)); // Checks if an element is in the array
+// console.log(myArr.indexOf(3)); // Returns the index of an element in the array 
+// const newArr = myArr.join(); // Joins all elements of the array into a string
+console.log(newArr); 
