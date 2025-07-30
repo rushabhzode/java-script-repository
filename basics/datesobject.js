26a2da870a0ec5e7765f95d72f1f1ef63bcb486a
rushabh <rushabhzode84@gmail.com>
2025-07-30 15:57:49 +0530

    learn about Date object and its methods

---
+++ basics/datesobject.js
@@ -0,0 +1,13 @@
+//Dates is a object in the java script and has multiple methods
+//every method is a function but not every function is a method
+//function is a blcok of code that performs a specific task
+//method is a function that is associated with an object
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let todaysDate = new Date(2023,0,23,5,4);
+console.log(todaysDate.toLocaleString());
