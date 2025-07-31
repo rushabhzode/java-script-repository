37df8907e5b9599da6bd59bcc6830192e4d4218e
rushabh <rushabhzode84@gmail.com>
2025-08-01 00:15:59 +0530

    update object file

--- 02basics/object.js
+++ 02basics/object.js
@@ -83,37 +83,37 @@
 //     },
 // ]
 
-// users[1].email
-// console.log(tinderUser);
+users[1].email
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
-// const course = {
-//     coursename: "js in hindi",
-//     price: "999",
-//     courseInstructor: "hitesh"
-// }
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
 
-// course.courseInstructor
+course.courseInstructor
 
-//const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course
 
-// console.log(courseInstructor);
-//console.log(instructor);
+console.log(courseInstructor);
+console.log(instructor);
 
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
+{
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+}
 
-// [
-//     {},
-//     {},
-//     {}
+[
+    {},
+    {},
+    {}
 // ]
