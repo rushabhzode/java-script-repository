943f244e03e994e9b088cc95ea29c674b59d5caf
rushabh <rushabhzode84@gmail.com>
2025-07-31 15:47:46 +0530

    learn object literals

---
+++ 02basics/object.js
@@ -0,0 +1,12 @@
+//creating object and acces it
+
+const person = {
+    name: "Rushabh",
+    age: 28,
+    email:"rushabhzode@gmail.com",
+    location: "India",
+}
+console.log(person["name"]); // Accessing property using bracket notation
+console.log(person["age"]); // Accessing property using bracket notation
+console.log(person["email"]); 
+console.log(person["location"]);
